889e0ccbd344ff2cb01f45efe75ee2e0c91ae91a
monukr2310 <monukr377@gmail.com>
2021-03-20 12:32:51 +0530

    changes3

---
+++ build/precache-manifest.6732a8ed173ec6bfe55f1fdd5338d70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb9591fe45cc46b2760dc70e887145e",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "007d1b0657a5fb01044e",
+    "url": "/React-Calculator/static/css/main.48fca561.chunk.css"
+  },
+  {
+    "revision": "d30355decc3e6dd951e2",
+    "url": "/React-Calculator/static/js/2.5d4819b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.5d4819b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007d1b0657a5fb01044e",
+    "url": "/React-Calculator/static/js/main.c61ffdc4.chunk.js"
+  },
+  {
+    "revision": "aa11618cf415fd926f7f",
+    "url": "/React-Calculator/static/js/runtime-main.d784676e.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import CalDesign from "./CalculatorDesign";
+
+
+
+
+ReactDom.render(
+  <>
+    
+       <CalDesign/>
+      
+  </>
+  ,
+  document.getElementById("root")
+);
